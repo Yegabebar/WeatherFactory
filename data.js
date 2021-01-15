a13519f060d5ba26e40a4f3c47049b2783c4bf47
Yegabebar <m.barbegayet@gmail.com>
2021-01-15 09:33:11 +0100

    Refactorisation du code: fonction de requête API - OK

--- data.js
+++ data.js
@@ -3,6 +3,15 @@ var illustration = document.getElementById("illustration");
 var jourCourant = document.getElementById("jourCourant");
 var semaine = document.getElementById("semaine");
 
+async function getResponse(url) {
+    const response = await fetch(url);
+    if (response.status != 200) {
+        alert("Try again later");
+    } else {
+        var data = await response.json();
+        return data;
+    }
+}
 
 function unixTimeStampToHour(t) {
     var dt = new Date(t * 1000);
@@ -13,7 +22,7 @@ function unixTimeStampToHour(t) {
 function getDay(t) {
     var dt = new Date(t * 1000);
     //Récupération numéro de jour
-    var day = dt.getDay();
+    let day = dt.getDay();
     //Correspondance numéro de jour avec nom jour pour affichage ultérieur
     switch (day) {
         case 0: day = "Lun";
@@ -41,49 +50,44 @@ async function recupMeteoJour(position) {
     let url = 'https://api.openweathermap.org/data/2.5/onecall?lat='
         + position.coords.latitude + '&lon='
         + position.coords.longitude + '&exclude=minutely,alerts&appid=bea2c2ef5da3e6bbecace5807f66ff95&units=metric';
+    
+    let data = await getResponse(url);
+    
+    //Vidage de la div de la page HTML où seront insérées les données
+    //Pour éviter l'empilement de données dans les div
+    blockInfo.innerHTML = "";
 
-    const response = await fetch(url);
-    if (response.status != 200) { //Si le retour n'est pas Ok, affichage erreur
-        console.log("Réponse API non OK");
-    } else {
-        //Vidage de la div de la page HTML où seront insérées les données
-        //Pour éviter l'empilement de données dans les div
-        blockInfo.innerHTML = "";
+    //Variables issues de la réponse JSON
+    let { current } = data;
+    let temp = current["temp"];
+    let weather = current["weather"]["0"]["main"];
 
-        //Variables issues de la réponse JSON
-        const data = await response.json();
-        let { current } = data;
-        let temp = current["temp"];
-        let weather = current["weather"]["0"]["main"];
+    //Création du div principal
+    let divDayInfo = document.createElement("div");
+    //Création des sous-div
+    let imageMeteo = document.createElement("img");
+    let nomVille = document.createElement("div");
+    let temperature = document.createElement("div");
+    let descImageMeteo = document.createElement("div");
+    
 
-        //Création du div principal
-        let divDayInfo = document.createElement("div");
-        //Création des sous-div
-        let imageMeteo = document.createElement("img");
-        let nomVille = document.createElement("div");
-        let temperature = document.createElement("div");
-        let descImageMeteo = document.createElement("div");
-        
+    //ajout de l'icone correspondant à la description du temps
+    let pathIcon = iconWeather(weather);
 
-        //ajout de l'icone correspondant à la description du temps
-        let pathIcon = iconWeather(weather);
+    //Ecriture de valeurs dans chaque sous-div
+    imageMeteo.innerHTML = '<img id = "mainImage" src ="' + pathIcon + '" alt = " ' + weather + '" />';
+    nomVille.innerHTML = '<div id="flexItem">'+titreVille+'</div>';
+    temperature.innerHTML = '<div id="flexItem">'+Math.floor(temp) + '°C</div>';
+    descImageMeteo.innerHTML = '<div id="flexItem">'+weather+'</div>';
 
-        //Ecriture de valeurs dans chaque sous-div
-        imageMeteo.innerHTML = '<img id = "mainImage" src ="' + pathIcon + '" alt = " ' + weather + '" />';
-        nomVille.innerHTML = '<div id="flexItem">'+titreVille+'</div>';
-        temperature.innerHTML = '<div id="flexItem">'+Math.floor(temp) + '°C</div>';
-        descImageMeteo.innerHTML = '<div id="flexItem">'+weather+'</div>';
+    //Insertion de chaque sous-div dans le div principal
+    divDayInfo.innerHTML = nomVille.innerHTML;
+    divDayInfo.innerHTML += temperature.innerHTML;
+    divDayInfo.innerHTML += descImageMeteo.innerHTML;
 
-        //Insertion de chaque sous-div dans le div principal
-        divDayInfo.innerHTML = nomVille.innerHTML;
-        divDayInfo.innerHTML += temperature.innerHTML;
-        divDayInfo.innerHTML += descImageMeteo.innerHTML;
-
-        //Ajout du div principal dans la page HTML
-        illustration.innerHTML = imageMeteo.innerHTML;
-        blockInfo.append(divDayInfo);
-
-    }
+    //Ajout du div principal dans la page HTML
+    illustration.innerHTML = imageMeteo.innerHTML;
+    blockInfo.append(divDayInfo);
 
 }
 
@@ -119,17 +123,11 @@ async function recupVille(position) {
         + position.coords.latitude + '&lon='
         + position.coords.longitude + '&appid=bea2c2ef5da3e6bbecace5807f66ff95&units=metric';
     
-        //Récupération résultat de fonction asynchrone
-    const response = await fetch(url);
+    //Récupération résultat de fonction asynchrone
+    let data = await getResponse(url);
+    let { name } = data;
+    return name;
 
-    if (response.status != 200) {
-        console.log("Réponse API non OK");
-    } else {
-
-        const data = await response.json();
-        let { name } = data;
-        return name;
-    }
 }
 
 async function recup3Heures(position) {
@@ -139,46 +137,40 @@ async function recup3Heures(position) {
         + position.coords.longitude + '&exclude=minutely,alerts&appid=bea2c2ef5da3e6bbecace5807f66ff95&units=metric';
     
     //Récupération résultat de fonction asynchrone
-    const response = await fetch(url);
+    data = await getResponse(url);
+    //Récupération des valeurs dans la réponse JSON
+    let { hourly } = data;
+    let { temp } = data;
 
-    if (response.status != 200) {
-        console.log("Réponse API non OK");
-    } else {
-        //Récupération résultat de fonction asynchrone
-        const data = await response.json();
+    //Vidage de la div de la page HTML où seront insérées les données
+    jourCourant.innerHTML = "";
 
-        let { hourly } = data;
-        let { temp } = data;
+    for (i = 1; i <= 21; i = i + 3) {
+        //Définition variables timestamp et température
+        time = hourly[i]["dt"];
+        temp = hourly[i]["temp"];
 
-        //Vidage de la div de la page HTML où seront insérées les données
-        jourCourant.innerHTML = "";
+        //Transformation du timestamp en valeur lisible
+        var heure = unixTimeStampToHour(time);
 
-        for (i = 1; i <= 21; i = i + 3) {
-            //Définition variables timestamp et température
-            time = hourly[i]["dt"];
-            temp = hourly[i]["temp"];
+        //Création du div principal
+        var creneau = document.createElement("div");
+        //Création des sous-div
+        var creneauH = document.createElement("div");
+        var creneauT = document.createElement("div");
 
-            //Transformation du timestamp en valeur lisible
-            var heure = unixTimeStampToHour(time);
+        //Ecriture de valeurs dans chaque sous-div
+        creneauH.innerHTML = "<div class='flexItem'>" + heure + "h </div>";
+        creneauT.innerHTML = "<div class='flexItem'>" + Math.floor(temp) + "°C </div>";
+        
+        //Insertion de chaque sous-div dans le div principal
+        creneau.innerHTML = creneauH.innerHTML;
+        creneau.innerHTML += creneauT.innerHTML;
 
-            //Création du div principal
-            var creneau = document.createElement("div");
-            //Création des sous-div
-            var creneauH = document.createElement("div");
-            var creneauT = document.createElement("div");
-    
-            //Ecriture de valeurs dans chaque sous-div
-            creneauH.innerHTML = "<div class='flexItem'>" + heure + "h </div>";
-            creneauT.innerHTML = "<div class='flexItem'>" + Math.floor(temp) + "°C </div>";
-            
-            //Insertion de chaque sous-div dans le div principal
-            creneau.innerHTML = creneauH.innerHTML;
-            creneau.innerHTML += creneauT.innerHTML;
-
-            //Ajout du div principal dans la page HTML
-            jourCourant.append(creneau);
-        }
+        //Ajout du div principal dans la page HTML
+        jourCourant.append(creneau);
     }
+    
 }
 
 async function recup7Jours(position) {
@@ -188,53 +180,48 @@ async function recup7Jours(position) {
         + position.coords.longitude + '&exclude=minutely,alerts&appid=bea2c2ef5da3e6bbecace5807f66ff95&units=metric';
     
     //Récupération résultat de fonction asynchrone
-    const response = await fetch(url);
+    data = await getResponse(url);
+    //Récupération des valeurs dans la réponse JSON
+    let { daily } = data;
+    //Vidage de la div de la page HTML où seront insérées les données
+    semaine.innerHTML = "";
 
-    if (response.status != 200) {
-        console.log("catch api pas bon");
-    } else {
-        //Récupération résultat de fonction asynchrone
-        const data = await response.json();
-        let { daily } = data;
-        //Vidage de la div de la page HTML où seront insérées les données
-        semaine.innerHTML = "";
+    for (i = 0; i <= 6; i++) {
 
-        for (i = 0; i <= 6; i++) {
+        let pathIcon = iconWeather(description);
+        var day = daily[i]["dt"];
+        day = getDay(day);
+        var temp = daily[i]["temp"]["day"];
+        var description = daily[i]["weather"]["0"]["main"];
+        
+        //Création du div principal
+        var jour = document.createElement("div"); 
+        //Création des sous-div
+        var jourIcon = document.createElement("img");
+        var jourJ = document.createElement("div");        
+        var jourT = document.createElement("div");
+        
+        console.log('Debug: '+pathIcon);
 
-            let pathIcon = iconWeather(description);
-            var day = daily[i]["dt"];
-            day = getDay(day);
-            var temp = daily[i]["temp"]["day"];
-            var description = daily[i]["weather"]["0"]["main"];
-            
-            //Création du div principal
-            var jour = document.createElement("div"); 
-            //Création des sous-div
-            var jourIcon = document.createElement("img");
-            var jourJ = document.createElement("div");        
-            var jourT = document.createElement("div");
-            
-            console.log('Debug: '+pathIcon);
+        //Ecriture de valeurs dans chaque sous-div
+        jourJ.innerHTML = "<div class='flexItem'>" + day + "</div>";         
+        jourT.innerHTML = "<div class='flexItem'>" + Math.floor(temp) + "°C </div>";
+        jourIcon.innerHTML = '<img class = "smallImg" src ="' + pathIcon + '" alt = " ' + description + '" />';
 
-            //Ecriture de valeurs dans chaque sous-div
-            jourJ.innerHTML = "<div class='flexItem'>" + day + "</div>";         
-            jourT.innerHTML = "<div class='flexItem'>" + Math.floor(temp) + "°C </div>";
-            jourIcon.innerHTML = '<img class = "smallImg" src ="' + pathIcon + '" alt = " ' + description + '" />';
+        //Insertion de chaque sous-div dans le div principal
+        jour.innerHTML = jourJ.innerHTML;
+        jour.innerHTML += jourT.innerHTML;
+        jour.innerHTML += jourIcon.innerHTML;
+        
+        //Ajout du div principal dans la page HTML
+        semaine.append(jour);
 
-            //Insertion de chaque sous-div dans le div principal
-            jour.innerHTML = jourJ.innerHTML;
-            jour.innerHTML += jourT.innerHTML;
-            jour.innerHTML += jourIcon.innerHTML;
-            
-            //Ajout du div principal dans la page HTML
-            semaine.append(jour);
-
-        }
     }
 }
 
 if ("geolocation" in navigator) {
     navigator.geolocation.watchPosition((position) => {
+        var data;
         //Lancement du script dès acceptation partage de position par l'utilisateur
         recupMeteoJour(position);
         recup3Heures(position);
